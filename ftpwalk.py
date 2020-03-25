701aa31cd6a8d155d51a2b78a56935f65153f4a3
blankaII <blanka00@gmail.com>
2020-03-25 11:26:53 +0800

    Append class SFTPWalk

--- ftpwalk.py
+++ ftpwalk.py
@@ -1,17 +1,37 @@
-import os
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 17:27:21 2020
 
+original @author: Kasramvd
 
-class ftp_walker(object):
-    def __init__(self, connection, root):
+https://xbuba.com/questions/31465199
+https://github.com/kasramvd/FTPwalk
+
+modify: blankachang
+"""
+
+from os import path as ospath
+import stat
+
+class FTPWalk:
+    """
+    This class is contain corresponding functions for traversing the FTP
+    servers using BFS algorithm.
+    """
+    def __init__(self, connection):
         self.connection = connection
-        self.root = root
 
     def listdir(self, _path):
+        """
+        return files and directory names within a path (directory)
+        """
+
         file_list, dirs, nondirs = [], [], []
         try:
             self.connection.cwd(_path)
         except Exception as exp:
-            print "the current path is : ", self.connection.pwd(), exp.__str__(), _path
+            print ("the current path is : ", self.connection.pwd(), exp.__str__(),_path)
             return [], []
         else:
             self.connection.retrlines('LIST', lambda x: file_list.append(x.split()))
@@ -23,13 +43,58 @@ class ftp_walker(object):
                     nondirs.append(name)
             return dirs, nondirs
 
-    def Walk(self, top, path=''):
-        dirs, nondirs = self.listdir(top)
-        yield (path or top), dirs, nondirs
-        path = top
+    def walk(self, path='/'):
+        """
+        Walk through FTP server's directory tree, based on a BFS algorithm.
+        """
+        dirs, nondirs = self.listdir(path)
+        yield path, dirs, nondirs
         for name in dirs:
-            path = os.path.join(path, name)
-            for x in self.Walk(name, path):
-                yield x
+            path = ospath.join(path, name)
+            yield from self.walk(path)
+            # In python2 use:
+            # for path, dirs, nondirs in self.walk(path):
+            #     yield path, dirs, nondirs
             self.connection.cwd('..')
-            path = os.path.dirname(path)
+            path = ospath.dirname(path)
+            
+class SFTPWalk:
+    """
+    base on https://github.com/kasramvd/FTPwalk::FTPWalk
+    """
+    def __init__(self, connection):
+        self.connection = connection
+
+    def listdir(self, _path):
+        """
+        return files and directory names within a path (directory)
+        """
+
+        dirs, nondirs = [], []
+        try:
+            self.connection.chdir(_path)
+        except Exception as exp:
+            print ("the current path is : ", self.connection.pwd(), exp.__str__(),_path)
+            return [], []
+        else:
+            for entry in self.connection.listdir_attr():
+                if stat.S_ISDIR(entry.st_mode):
+                    dirs.append(entry.filename)
+                else:
+                    nondirs.append(entry.filename)
+            return dirs, nondirs
+
+    def walk(self, path='/'):
+        """
+        Walk through FTP server's directory tree, based on a BFS algorithm.
+        """
+        dirs, nondirs = self.listdir(path)
+        yield path, dirs, nondirs
+        for name in dirs:
+            path = ospath.join(path, name)
+            yield from self.walk(path)
+            # In python2 use:
+            # for path, dirs, nondirs in self.walk(path):
+            #     yield path, dirs, nondirs
+            self.connection.chdir('..')
+            path = ospath.dirname(path)
